944d3a287cc7f38b93b483ade38fc1d4ee1cfaa9
coarse knife <37704064-9au40yl1j@users.noreply.replit.com>
2024-11-10 04:11:34 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import React from 'react'
+import { useId } from "react"
+import { cn } from "@/lib/utils"
+
+interface DotPatternProps {
+  width?: any
+  height?: any
+  x?: any
+  y?: any
+  cx?: any
+  cy?: any
+  cr?: any
+  className?: string
+  [key: string]: any
+}
+
+function DotPattern({
+  width = 16,
+  height = 16,
+  x = 0,
+  y = 0,
+  cx = 1,
+  cy = 1,
+  cr = 1,
+  className,
+  ...props
+}: DotPatternProps) {
+  const id = useId()
+
+  return (
+    <svg
+      aria-hidden="true"
+      className={cn(
+        "pointer-events-none absolute inset-0 h-full w-full fill-neutral-400/80",
+        className,
+      )}
+      {...props}
+    >
+      <defs>
+        <pattern
+          id={id}
+          width={width}
+          height={height}
+          patternUnits="userSpaceOnUse"
+          patternContentUnits="userSpaceOnUse"
+          x={x}
+          y={y}
+        >
+          <circle id="pattern-circle" cx={cx} cy={cy} r={cr} />
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id})`} />
+    </svg>
+  )
+}
+
+export function BlackBackgroundWithDots() {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-black">
+      <h1 className="z-10 text-center text-5xl font-medium tracking-tighter text-white">
+        Hello world
+      </h1>
+      <DotPattern
+        className={cn(
+          "[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]",
+          "fill-white/20"
+        )}
+        width={16}
+        height={16}
+        cx={1}
+        cy={1}
+        cr={1}
+      />
+    </div>
+  )
+}
+
+export default function Home() {
+  return (
+    <main>
+      <BlackBackgroundWithDots/>
+    </main>
+  )
+}
